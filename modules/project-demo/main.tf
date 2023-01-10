b31b830c704f59382e2aeceb514778ccd0b25442
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-10 14:32:27 +0100

    feat(modules/project-demo): Create module form whole project

---
+++ modules/project-demo/main.tf
@@ -0,0 +1,21 @@
+variable "config" {}
+
+data "digitalocean_ssh_key" "default" {
+  name = var.config.ssh_key_name
+}
+
+module "vm" {
+  for_each = var.config.vms
+
+  source = "../../modules/vm"
+  name   = "${var.config.env}-${each.key}"
+  ssh_keys = [
+    data.digitalocean_ssh_key.default.id,
+  ]
+}
+
+output "ips" {
+  value = {
+    for k, v in module.vm : k => v.ipv4_address
+  }
+}
